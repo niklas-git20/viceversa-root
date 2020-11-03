7f85e7f3829d983547b784ec0feed1a3acee691c
Nik <boyko.nikolay@gmail.com>
2020-11-03 18:21:05 +0200

    words count

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def viceversa(request):
 	return render(request, 'viceversa.html')
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	len_text = len(user_text.split())
 	rev_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext': user_text,'reversedtext':rev_text})
+	return render(request, 'reverse.html', {'usertext': user_text,
+		'reversedtext':rev_text,
+		'lentext':len_text})
